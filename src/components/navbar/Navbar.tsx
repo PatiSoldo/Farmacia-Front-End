254027b0caf542c7622d3e3ff0a4b4438056be7d
PatiSoldo <patriciasoldoalves@hotmail.com>
2023-10-17 15:39:23 -0300

    CRUD-categoria

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,29 +1,41 @@
-import React from 'react'
-import homeLogo from '../../assets/logo_farmacia 1.png'
-import userImage from '../../assets/user 1.png'
+import React, { useContext } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { AuthContext } from '../../contexts/AuthContext'
+
 
 
 function Navbar() {
- 
+  let navigate = useNavigate()
+
+  const { usuario, handleLogout } = useContext(AuthContext)
+
+  function logout() {
+      handleLogout()
+      alert('Usuário deslogado com sucesso')
+      navigate('/login')
+  }
+
+  let navbarComponent  //variavel usada no conteúdo sobre renderização condicional
   
 
   return (
     <>
-     < className='w-full bg-indigo-900 text-white flex justify-center py-4'>
+     <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
           <div className="container flex justify-between text-lg">
-            {/* <div className='text-2xl font-bold uppercase'>Farmácia</div> */}
-            <div className="flex justify-center ">
-            <img src={homeLogo} alt="logo farmacia" className='w-2/3' />
-          </div>
+          <Link to='/home' className='text-2xl font-bold uppercase'>Blog Pessoal</Link>
 
+          <Link to='/home' className="flex justify-center">
+            <img src={homeLogo} alt="" className='w-2/3' />
+            </Link>
             <div className='flex gap-4'>
-              <div className='hover:underline'>Categorias</div>
-              <div className='hover:underline'>Cadastrar categoria</div>
-              {/* <div className='hover:underline'>Perfil</div> */}
-              <div className="flex justify-center ">
-              <img src={userImage} alt="icone de usuário " className='w-2/3' />
-              </div>
+            <Link to='/categoria' className='hover:underline'>Categorias</Link>
+            <Link to='/cadastroCategoria' className='hover:underline'>Cadastrar categoria</Link>
+            <Link to='' onClick={logout} className="flex justify-center ">
+                <img src={userImage} alt="" className='w-11 h-12' />
+            </Link>
+             
             </div>
+          </div>
         </div>
     </>
   )
